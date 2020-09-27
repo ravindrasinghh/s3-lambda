766d592bf23d4f6d2fea6a858c095195cb83cf49
Ravindra <ravindra.singh@aux.dkatalis.com>
2020-09-27 11:43:22 +0530

    added lambda function

--- lambda_function.py
+++ lambda_function.py
@@ -1,4 +1,5 @@
 import boto3
+import csv
 s3_client = boto3.client("s3")
 
 dynamodb = boto3.resource('dynamodb')
@@ -9,6 +10,15 @@ def lambda_handler(event, context):
     s3_file_name = event['Records'][0]['s3']['object']['key']
     resp = s3_client.get_object(Bucket=bucket_name,Key=s3_file_name)
     data = resp['Body'].read().decode("utf-8")
-    
-    print(data)
-    
+    employees = data.split("\n")
+    for emp in employees:
+        print(emp)
+    #     emp_data = emp.split(",")
+    # #added to dynamodb
+    #     table.put_item(
+    #         Item = {
+    #             "id" : emp_data[0],
+    #             "name" : emp_data[1],
+    #             "location" : emp_data[2]
+    #         }
+    #     )        
